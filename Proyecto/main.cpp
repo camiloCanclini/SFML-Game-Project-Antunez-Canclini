82fa80cf7cea0ac7fe90a9737690eec978cfa294
Camilo <camilocanclini@gmail.com>
2022-06-17 22:12:31 -0300

    PRUEBA VSCOM

--- Proyecto/main.cpp
+++ Proyecto/main.cpp
@@ -1,6 +1,5 @@
 #include<SFML/Graphics.hpp>
 #include<SFML/Audio.hpp>
-#include<SFML/Audio/Music.hpp>
 #include "player.cpp"
 #include "background.cpp"
 #include "enemy.cpp"
@@ -15,71 +14,72 @@ int main(void){
     la resolucion*/ 
 
     float actualResolution[2] = {1080,720}; // resolucion actual
-    int baseResolution[2] = {1080,720}; // resolucion base sobre la que se construyó el programa
-    int aspectRatioX = (baseResolution[0]/actualResolution[0]); // relacion de la pantalla en x
-    int aspectRatioY = (baseResolution[1]/actualResolution[1]); // relacion de la pantalla en y
-    int aspectRelation[2] = {aspectRatioX,aspectRatioY};  // se guarda los valores en un array
  
     RenderWindow window;
     window.create(VideoMode(actualResolution[0], actualResolution[1]), "Game");
-    window.clear(Color::Red);
-    window.setVerticalSyncEnabled(true); // Los fps se sincronizan con los de la pantalla
+    //window.setVerticalSyncEnabled(true); // Los fps se sincronizan con los de la pantalla
     //window.setFramerateLimit(60);
-    Clock clock;
+    /*Clock clock;
     clock.getElapsedTime();
     Time timer;
-    timer.asSeconds(); 
+    timer.asSeconds();
+    */ 
     Background background("Resources/Textures/Environment/room.png", window);
-    Player player(100,100,"Resources/Textures/Player/test.png",window);
-    float stepMove = 10.f, stepAceleration = 10.f; 
+    //Player player(100,100,"Resources/Textures/Player/test.png");
+    Texture texturePlayer;
+    texturePlayer.loadFromFile("Resources/Textures/Enemies/pythonBlue1.png");
+    Sprite player;
+    player.setTexture(texturePlayer);
+    //Se hace regla de 3 para sacar escala para los valores declarados arriba:
+    //((dimesionEnPixelesDeseada)/TamañoPx(textura.getSize())*escalaActual(1)= nuevaEscala)
+    //player.setScale(100.f/texturePlayer.getSize().x,100.f/texturePlayer.getSize().y);
+    float stepMove = 3, stepAceleration = 10.f; 
     Enemy enemy(400,400,"Resources/Textures/Enemies/pythonBlue1.png",window);
     Music backgroundMusic;
+
     if (!backgroundMusic.openFromFile("Resources/Music/QuincasMoreira-Robot City.ogg")){
         return -1; // error
     }else{
         backgroundMusic.setVolume(5.0f);
         backgroundMusic.play();
     }
-    window.display();
     //Events---------------------------------------------------------------------
     Event event; //Se crea el evento principal
     while (window.isOpen()){
-        timer = clock.getElapsedTime();
+        //timer = clock.getElapsedTime();
         //cout << (int)timer.asSeconds() << endl;     
-        window.clear();
-        background.draw(window);
-        player.draw(window);
-        enemy.draw(window);
         while (window.pollEvent(event)){ //Se inicia el MAINLOOP
-            switch (event.type){
-                case Event::Closed:
-                    window.close();
-                    break;
-            }   
+            if (event.type == Event::Closed){
+                window.close();
+                break;
+            }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
             {
                 // left key is pressed: move our character
-                player.moveTo(0.f, -stepMove, window);
+                player.move(0, -stepMove);
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
             {
                 // left key is pressed: move our character
-                player.moveTo(0.f, stepMove, window);
+                player.move(0, stepMove);
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
             {
                 // left key is pressed: move our character
-                player.moveTo(stepMove, 0.f, window);
+                player.move(stepMove, 0);
             }
             if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
             {
                 // left key is pressed: move our character
-                player.moveTo(-stepMove, 0.f, window);
+                player.move(-stepMove, 0);
             }
         }
-        x = Player::acelerationPhysics(x);
+        /*x = Player::acelerationPhysics(x);
         y = Player::acelerationPhysics(y);
-        player.move(x,y);
+        player.move(x,y);*/
+        
+        window.clear();
+        window.draw(player);
         window.display();
     }
 }
--- Proyecto/player.cpp
+++ Proyecto/player.cpp
@@ -3,32 +3,30 @@
 using namespace sf;
 using namespace std;
 
-class Player
+class Player : public Drawable
 {
 public:
-    Player(int positionx, int positiony, string textureSourcePath, sf::RenderWindow &window){
-        cout << "Window?" << &window << endl;
+    Player(int positionx, int positiony, string textureSourcePath){
         //Texture texturePlayer;
         texturePlayer.loadFromFile(textureSourcePath);
-        texturePlayer.setSmooth(true);
         player.setPosition(positionx,positiony);
         player.setTexture(texturePlayer);
         //Se hace regla de 3 para sacar escala para los valores declarados arriba:
         //((dimesionEnPixelesDeseada)/TamañoPx(textura.getSize())*escalaActual(1)= nuevaEscala)
         player.setScale(playerSizeX/texturePlayer.getSize().x,playerSizeY/texturePlayer.getSize().y);
-        window.draw(player);
     }
     ~Player(){
         cout << "El objeto se deberia borrar" << endl;
     }
-
+    void draw(RenderTarget &target, RenderStates states) const override
+    {
+        target.draw(player,states);
+    }
     void attack(Vector2<int> &direction){
 
     }
-    void moveTo(float x ,float y,sf::RenderWindow &window){
-        cout << x << " moviendo " << y << endl;
+    void moveTo(float x ,float y){
         player.move(x,y);
-        window.draw(player);
     }
     void healthChange(int point){
         health += point;
@@ -56,9 +54,6 @@ public:
     }
     void die(){
         
-    }
-    void draw(sf::RenderWindow &window){
-        window.draw(player);
     }
     float acelerationPhysics(float aceleration){
         float stepAceleration = 2, maxAceleration = 40;
